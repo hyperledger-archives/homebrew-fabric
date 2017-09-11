a49d588d24a968a470e23ee71d7929e4ce46f738
Erick Guan <fantasticfears@gmail.com>
2017-09-11 17:15:38 +0200

    fabric-tools 1.0.2

--- Formula/fabric-tools.rb
+++ Formula/fabric-tools.rb
@@ -5,8 +5,8 @@
 class FabricTools < Formula
   desc "Hyperledger Fabric native binaries installer"
   homepage "https://hyperledger.org/projects/fabric"
-  url "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/darwin-amd64-1.0.0/hyperledger-fabric-darwin-amd64-1.0.0.tar.gz"
-  sha256 "8b03d1e7d4dd287355a7d5021434e2ffab76c90b8359c8fe3f7f11bcae9d2dba"
+  url "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/darwin-amd64-1.0.2/hyperledger-fabric-darwin-amd64-1.0.2.tar.gz"
+  sha256 "b0ef8362cacca4d0a83be761fdec379236d32bc38e1cadfec8aeab312f342deb"
 
   def install
     bin.install "cryptogen"
