082a042e08f8a7a1af0886fe629872e0fe471a58
Ry Jones <ry@linux.com>
2020-02-19 13:29:49 -0800

    Add tap for v2.0
    Updated tap for v1.4
    
    Signed-off-by: Ry Jones <ry@linux.com>

--- Formula/fabric-tools@1.4.0.rb
+++ Formula/fabric-tools@1.4.0.rb
@@ -2,11 +2,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-class FabricToolsAT144 < Formula
+class FabricToolsAT140 < Formula
   desc "Hyperledger Fabric native binaries installer"
   homepage "https://hyperledger.org/projects/fabric"
-  url "https://github.com/hyperledger/fabric/releases/download/v1.4.4/hyperledger-fabric-darwin-amd64-1.4.4.tar.gz"
-  sha256 "6ba9701a8913eda4e2d226f07bd0e3bb52effa143cd040bbdb6c877f6cd735a8"
+  url "https://github.com/hyperledger/fabric/releases/download/v1.4.6/hyperledger-fabric-darwin-amd64-1.4.6.tar.gz"
+  sha256 "efc4c1139935c73f7d3af2995822e7f3bee07512e09ddceb00240d3fc1b769e5"
 
   def install
     bin.install "bin/cryptogen"
--- scripts/update-homebrew.sh
+++ scripts/update-homebrew.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
 VERSION=${1:-1.4.4}
 URL="https://github.com/hyperledger/fabric/releases/download/v${VERSION}/hyperledger-fabric-darwin-amd64-${VERSION}.tar.gz"
-SHA=`curl -sSL ${URL} | shasum -a 256`
+SHA=`curl -f -sSL ${URL} | shasum -a 256`
 echo 'url "'${URL}'"'
 echo 'sha256 "'${SHA:0:64}'"'
