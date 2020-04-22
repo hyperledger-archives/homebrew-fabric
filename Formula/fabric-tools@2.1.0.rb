ebd490c15773e57008e1833f26f72e4a64aad71f
Ry Jones <ry@linux.com>
2020-04-22 03:20:29 -0700

    Add 2.1 tap
    Signed-off-by: Ry Jones <ry@linux.com>

---
+++ Formula/fabric-tools@2.1.0.rb
@@ -0,0 +1,22 @@
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+class FabricToolsAT200 < Formula
+  desc "Hyperledger Fabric native binaries installer"
+  homepage "https://hyperledger.org/projects/fabric"
+  url "https://github.com/hyperledger/fabric/releases/download/v2.1.0/hyperledger-fabric-darwin-amd64-2.1.0.tar.gz"
+  sha256 "7e538b8532e914768139e5da58176c99d22deabb88fe09ab93c391e573b3aefe"
+
+  def install
+    bin.install "bin/cryptogen"
+    bin.install "bin/configtxgen"
+    bin.install "bin/configtxlator"
+  end
+
+  test do
+    system "#{bin}/configtxgen --help"
+    system "#{bin}/configtxlator --help"
+    system "#{bin}/cryptogen --help"
+  end
+end
