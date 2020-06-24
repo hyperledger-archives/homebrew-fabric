497624789b4708f4669fa9b036b71a79b474fce4
Ry Jones <ry@linux.com>
2020-06-24 11:07:09 -0700

    Update 2.1.0 to 2.1.1
    Signed-off-by: Ry Jones <ry@linux.com>

--- Formula/fabric-tools@2.1.0.rb
+++ Formula/fabric-tools@2.1.0.rb
@@ -6,8 +6,8 @@
 class FabricToolsAT210 < Formula
   desc "Hyperledger Fabric native binaries installer"
   homepage "https://hyperledger.org/projects/fabric"
-  url "https://github.com/hyperledger/fabric/releases/download/v2.1.0/hyperledger-fabric-darwin-amd64-2.1.0.tar.gz"
-  sha256 "7e538b8532e914768139e5da58176c99d22deabb88fe09ab93c391e573b3aefe"
+  url "https://github.com/hyperledger/fabric/releases/download/v2.1.1/hyperledger-fabric-darwin-amd64-2.1.1.tar.gz"
+  sha256 "d1ee4a86bee27ddb97ca23054e1737ad90fff3db44155f336417876dccb198da"
 
   def install
     bin.install "bin/cryptogen"
